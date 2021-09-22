90e13bcfc42ec33ab203d86c1e416e4b13c452a3
Ousmane Fall <fallousmane162019@gmail.com>
2021-09-22 09:50:27 +0000

    movie app hooks

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Card, CardTitle, CardImg, CardBody, CardText } from 'reactstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, CardTitle, CardImg, CardBody, CardText } from "reactstrap";
 
-import Rate from './Rate';
+import Rate from "./Rate";
 
 const MovieCard = ({ movie: { title, posterUrl, description, rate } }) => {
   return (
-    <div className='col-4 mt-2'>
+    <div className="col-4 mt-2">
       <Card style={{ height: `500px` }}>
         <CardTitle>
-          <div className='row justify-content-center mt-2'>
+          <div className="row justify-content-center mt-2">
             <h6>{title}</h6>
           </div>
         </CardTitle>
         <CardImg
           top
-          width='100%'
+          width="100%"
           style={{ height: `300px` }}
           src={posterUrl}
-          alt='Card image cap'
+          alt="Card image cap"
         />
         <CardBody>
-          <div className='row'>
+          <div className="row">
             <CardText>{description}</CardText>
           </div>
-          <div className='row'>
+          <div className="row">
             <Rate rating={rate} />
           </div>
         </CardBody>
