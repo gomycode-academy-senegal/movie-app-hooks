c2dc2a9b088c68e8525a8907efe81e5f87afb504
Ousmane Fall <fallousmane162019@gmail.com>
2021-09-27 17:03:12 +0000

    add react-router-dom ajust

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import { moviesList } from "../assets/mookData";
 import Rate from "./Rate";
-import { Card, CardTitle, CardImg, CardBody, CardText } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardText,
+} from "reactstrap";
 
 const MovieItem = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
 
   const fetchData = async () => {
-    const items = await moviesList[id - 1]
-    setMovie(items)
+    const items = await moviesList[id - 1];
+    setMovie(items);
   };
 
   useEffect(() => {
@@ -19,29 +26,32 @@ const MovieItem = () => {
   return (
     <>
       <div className="col-4 mt-2">
-      <Card style={{ height: `500px` }}>
-        <CardTitle>
-          <div className="row justify-content-center mt-2">
-            <h6>{movie.title}</h6>
-          </div>
-        </CardTitle>
-        <CardImg
-          top
-          width="100%"
-          style={{ height: `300px` }}
-          src={movie.posterUrl}
-          alt="Card image cap"
-        />
-        <CardBody>
-          <div className="row">
-            <CardText>{movie.description}</CardText>
-          </div>
-          <div className="row">
-            <Rate rating={movie.rate} />
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+        <NavLink to="/">
+          <Button color="primary">Home</Button>
+        </NavLink>
+        <Card style={{ height: `500px` }}>
+          <CardTitle>
+            <div className="row justify-content-center mt-2">
+              <h6>{movie.title}</h6>
+            </div>
+          </CardTitle>
+          <CardImg
+            top
+            width="100%"
+            style={{ height: `300px` }}
+            src={movie.posterUrl}
+            alt="Card image cap"
+          />
+          <CardBody>
+            <div className="row">
+              <CardText>{movie.description}</CardText>
+            </div>
+            <div className="row">
+              <Rate rating={movie.rate} />
+            </div>
+          </CardBody>
+        </Card>
+      </div>
     </>
   );
 };
